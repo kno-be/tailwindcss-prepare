2d682d3ee1f1bb980981f842c1ae80a5bbc8685e
Bernardo Knoblauch <67606710+kno-be@users.noreply.github.com>
2021-06-18 00:17:19 -0300

    new: summary

---
+++ pages/art.js
@@ -0,0 +1,81 @@
+import React from 'react';
+
+export default function Art() {
+    const [open, setOpen] = React.useState(false);
+
+    return(
+
+        <div className="p-0">
+            <a href="/"><button className="bg-black w-full text-white border-2">HOME</button></a>
+
+            <div className="flex h-screen w-full p-5 bg-black justify-center">
+                
+                
+                    <div className="relative h-80 w-60 bg-blue-800"></div>
+                    <div></div>
+                    <div>
+                        
+                    </div>
+                
+                
+            </div>
+        </div>
+
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
